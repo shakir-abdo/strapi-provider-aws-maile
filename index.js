2684e115d1a3187bc4af8a52e499a9effb47d4b2
shakir abdo <shicolare1@gmail.com>
2023-05-19 14:50:10 +0300

    remove unnecessary console logging

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ module.exports = {
   init: (providerOptions = {}, settings = {}) => {
     const ses = require("node-ses");
     const client = ses.createClient(providerOptions);
-    console.log(providerOptions);
     return {
       send: async (options) => {
         options.message = options.text;
-
             return await new Promise((resolve, reject) => {
                 let payload = Object.assign(settings,
                   {
